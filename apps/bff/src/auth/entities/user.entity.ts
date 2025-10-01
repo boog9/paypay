69f1e335dea2089ac7f072a3b2821eb35c3873b5
boogie <53789468+boog9@users.noreply.github.com>
2025-10-02 00:51:02 +0300

    Remove redundant email normalization hook

--- apps/bff/src/auth/entities/user.entity.ts
+++ apps/bff/src/auth/entities/user.entity.ts
@@ -1,5 +1,4 @@
 import {
-  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -8,7 +7,6 @@ import {
   UpdateDateColumn
 } from 'typeorm';
 import { RefreshTokenEntity } from './refresh-token.entity';
-import { normalizeEmail } from '../email.utils';
 
 @Entity({ name: 'users' })
 export class UserEntity {
@@ -30,8 +28,4 @@ export class UserEntity {
   @OneToMany(() => RefreshTokenEntity, (token) => token.user)
   refreshTokens!: RefreshTokenEntity[];
 
-  @BeforeInsert()
-  normalizeEmailBeforeInsert(): void {
-    this.email = normalizeEmail(this.email);
-  }
 }
